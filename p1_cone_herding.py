e35b7b68dee8a51b926e94a0565513c36d8bea9d
Dom Barter <dominicbarter@gmail.com>
2018-12-11 12:06:42 +0000

    update

--- p1_cone_herding.py
+++ p1_cone_herding.py
@@ -1,5 +1,5 @@
 """__CONFIG__
-{"version":20,"widgetInfos":[{"hwid":"1","name":"LeftDrive","typeName":"motor","extraConfig":null,"bufferIndex":0},{"hwid":"2","name":"RightDrive","typeName":"motor_rp","extraConfig":null,"bufferIndex":1},{"hwid":"3","name":"TouchLed","typeName":"touch_led","extraConfig":null,"bufferIndex":2},{"hwid":"6","name":"MiddleUltra","typeName":"distance_cm","extraConfig":null,"bufferIndex":3},{"hwid":"7","name":"LeftColor","typeName":"color_hue","extraConfig":null,"bufferIndex":4},{"hwid":"8","name":"RightColor","typeName":"color_hue","extraConfig":null,"bufferIndex":5},{"hwid":"drivetrain","name":"dt","typeName":"drivetrain","extraConfig":{"leftMotorHwId":"1","rightMotorHwId":"2","wheelTravel":200,"trackWidth":254},"bufferIndex":6},{"hwid":"lcd","name":"lcd","typeName":"lcd","extraConfig":null,"bufferIndex":7},{"hwid":"sound","name":"sound","typeName":"sound","extraConfig":null,"bufferIndex":8},{"hwid":"btn_chk","name":"button_check","typeName":"face_button","extraConfig":null,"bufferIndex":9},{"hwid":"btn_up","name":"button_up","typeName":"face_button","extraConfig":null,"bufferIndex":10},{"hwid":"btn_down","name":"button_down","typeName":"face_button","extraConfig":null,"bufferIndex":11}]}"""
+{"version":20,"widgetInfos":[{"hwid":"1","name":"LeftDrive","typeName":"motor","extraConfig":null,"bufferIndex":0},{"hwid":"2","name":"RightDrive","typeName":"motor_rp","extraConfig":null,"bufferIndex":1},{"hwid":"3","name":"TouchLed","typeName":"touch_led","extraConfig":null,"bufferIndex":2},{"hwid":"4","name":"LeftColour","typeName":"color_hue","extraConfig":null,"bufferIndex":3},{"hwid":"5","name":"RightColour","typeName":"color_hue","extraConfig":null,"bufferIndex":4},{"hwid":"6","name":"MiddleUltra","typeName":"distance_cm","extraConfig":null,"bufferIndex":5},{"hwid":"drivetrain","name":"dt","typeName":"drivetrain","extraConfig":{"leftMotorHwId":"1","rightMotorHwId":"2","wheelTravel":200,"trackWidth":210},"bufferIndex":6},{"hwid":"lcd","name":"lcd","typeName":"lcd","extraConfig":null,"bufferIndex":7},{"hwid":"sound","name":"sound","typeName":"sound","extraConfig":null,"bufferIndex":8},{"hwid":"btn_chk","name":"button_check","typeName":"face_button","extraConfig":null,"bufferIndex":9},{"hwid":"btn_up","name":"button_up","typeName":"face_button","extraConfig":null,"bufferIndex":10},{"hwid":"btn_down","name":"button_down","typeName":"face_button","extraConfig":null,"bufferIndex":11}]}"""
 
 # external library imports ------------------
 
@@ -71,6 +71,8 @@ class Robot():
 
     def moveBy(self,distance): #move the robot forwards by a certain distance
 
+
+
         self.currentAngle = self.angle #gets current gyro reading
 
         self.cm = self.intify(distance) #gets the distance to travel in cm (0 dp)
@@ -93,6 +95,7 @@ class Robot():
                     self.resolveResult = self.resolveXY(self.x,self.y,30,self.currentAngle)
                     self.x , self.y = self.resolveResult.x , self.resolveResult.y
 
+                vexiq.lcd_write("Hello!")
                 return True #robot has been able to reach destination
 
             else: #if distance is not a multiple of 15
@@ -208,6 +211,10 @@ class Robot():
         else:
             return False
 
+    def move(self):
+        self.drivetrain.drive_until(30,300)
+        return True
+
 # -------------------------------------------
 
 # Hardware setup and port selection ---------
@@ -216,17 +223,19 @@ class Robot():
 LeftDrive   = vexiq.Motor(1)
 RightDrive  = vexiq.Motor(2, True) # Reverse Polarity
 TouchLed    = vexiq.TouchLed(3)
+LeftColour  = vexiq.ColorSensor(4) # hue
+RightColour = vexiq.ColorSensor(5) # hue
 MiddleUltra = vexiq.DistanceSensor(6, vexiq.UNIT_CM)
-LeftColor   = vexiq.ColorSensor(7) # hue
-RightColor  = vexiq.ColorSensor(8) # hue
-dt          = drivetrain.Drivetrain(LeftDrive, RightDrive, 200, 252) #wheel/track
+
+import drivetrain
+dt          = drivetrain.Drivetrain(LeftDrive, RightDrive, 200, 210)
 #endregion config
 
 # -------------------------------------------
 
 # Pre-program object creation ---------------
 
-robot = Robot(dt,LeftColor,RightColor,MiddleUltra) #create robot class
+robot = Robot(dt,LeftColour,RightColour,MiddleUltra) #create robot class
 
 # -------------------------------------------
 
@@ -240,46 +249,12 @@ while True:
     vexiq.lcd_write("Y Coord: " + str(robot.y),3)
 
     if TouchLed.is_touch():
-
+        
+        sys.sleep(0.25)
+        
         TouchLed.named_color(3) #orange
-
-        robot.moveBy(10)
-        robot.rotateTo(20)
-
-        robot.moveBy(10)
-        robot.rotateTo(40)
-
-        robot.moveBy(10)
-        robot.rotateTo(60)
-
-        robot.moveBy(10)
-        robot.rotateTo(80)
-
-        robot.moveBy(10)
-        robot.rotateTo(100)
-
-        robot.moveBy(10)
-        robot.rotateTo(120)
-
-        robot.moveBy(10)
-        robot.rotateTo(140)
-
-        robot.moveBy(10)
-        robot.rotateTo(160)
-
-        robot.moveBy(10)
+        
         robot.rotateTo(180)
-
-        """
-
-        robot.rotateTo(90)
-
-
-        robot.moveBy(30)
-        robot.rotateTo(180)
-        robot.moveBy(60)
-        robot.rotateTo(0)
-        robot.moveBy(30)"""
 
         sys.sleep(0.5)
 
