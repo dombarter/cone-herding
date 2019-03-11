96da5235f34434f05bfd264c83872324ce98d9a5
Dom Barter <dominicbarter@gmail.com>
2019-03-11 10:50:36 +0000

    Update packing.py

--- packing.py
+++ packing.py
@@ -1,9 +1,19 @@
 import math
 
-number = 6
+class coords:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
-estimate = round(math.sqrt((2*number) + 1) - 1)
-y = 0.5 * ( (2 * number) - (estimate ** 2) - (estimate) )
-x = 0.5 * ( (estimate ** 2) + (3 * estimate) - (2 * number) )
+def triNumbers(n):
 
-print(str(x) + ", " + str(y))
+    number = n
+
+    estimate = round(math.sqrt((2*number) + 1) - 1)
+    y = 0.5 * ( (2 * number) - (estimate ** 2) - (estimate) )
+    x = 0.5 * ( (estimate ** 2) + (3 * estimate) - (2 * number) )
+
+    return coords(x,y)
+
+result = triNumbers(26)
+print(str(result.x) + ", " + str(result.y))
